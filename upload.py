f137ea6edfd5526b22f9405a225a3943a3cd602c
Nicolai <nicolaiadil.aatif@gmail.com>
2022-10-04 10:35:46 +0200

    Lagt til program som kan laste opp fil

---
+++ upload.py
@@ -0,0 +1,30 @@
+from vars import SLACK_BOT_TOKEN, SLACK_APP_TOKEN
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(SLACK_BOT_TOKEN)
+logger = logging.getLogger(__name__)
+
+# The name of the file you're going to upload
+file_name = "img.jpg"
+# ID of channel that you want to upload file to
+channel_id = "general"
+
+
+try:
+    # Call the files.upload method using the WebClient
+    # Uploading files requires the `files:write` scope
+    result = client.files_upload(
+        channels=channel_id,
+        initial_comment="Sample text",
+        file=file_name
+    )
+    # Log the result
+    logger.info(result)
+
+except SlackApiError as e:
+    logger.error("Error uploading file: {}".format(e))
