88da18320fef3a0df10a26dbdeef7cc78a10c3d8
Edvard Sire <edvardsire@protonmail.com>
2022-10-15 17:22:32 +0200

    combine msg and upload to slackapi

--- slackapi.py
+++ slackapi.py
@@ -1,7 +1,21 @@
 from vars import SLACK_BOT_TOKEN
-import logging
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import logging
+
+
+def send_message(chat: str, message: str) -> None:
+    thing = WebClient(token=SLACK_BOT_TOKEN)
+
+    thing.chat_postMessage(
+        channel=chat,
+        text=message
+        # attachments=[
+        #         {
+        #             "fallback": "rot",
+        #             "image_url": link
+        #         }]
+    )
 
 
 # WebClient instantiates a client that can call API methods
@@ -10,14 +24,11 @@ def upload(image_URI, message, channel_id, log_state=True):
     client = WebClient(SLACK_BOT_TOKEN)
     logger = logging.getLogger(__name__)
 
-
     try:
         # Call the files.upload method using the WebClient
         # Uploading files requires the `files:write` scope
         result = client.files_upload(
-            channels=channel_id,
-            initial_comment=message,
-            file=image_URI
+            channels=channel_id, initial_comment=message, file=image_URI
         )
         # Log the result
         if log_state:
